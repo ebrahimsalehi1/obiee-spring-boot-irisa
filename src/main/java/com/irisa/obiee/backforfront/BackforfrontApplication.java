f351a3900d994b5e57d8d617bc91165b997cfa07
ebrahimsalehi1 <eeeee.bbbbb.iiiii@gmail.com>
2020-09-10 09:33:52 +0430

    first time make the back end project

---
+++ src/main/java/com/irisa/obiee/backforfront/BackforfrontApplication.java
@@ -0,0 +1,13 @@
+package com.irisa.obiee.backforfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackforfrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackforfrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irisa/obiee/backforfront/BackforfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irisa.obiee.backforfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackforfrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
