f15e72766faed391a1e827ac74165b4cb36f5c2e
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2021-04-22 13:20:57 +0430

    commit last changes from 1399

--- src/main/java/com/irisa/obiee/backforfront/obieeservices/UserController.java
+++ src/main/java/com/irisa/obiee/backforfront/obieeservices/UserController.java
@@ -11,6 +11,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.util.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -28,16 +34,19 @@ public class UserController {
 
         ResponseEntity<?> res = generalService.callWebService(url, HttpMethod.GET,null,true);
         User[] users = new Gson().fromJson(res.getBody().toString(), User[].class);
-        return new ResponseEntity<>(new Gson().toJson(users).toString(),HttpStatus.OK);
+//        List<User> list = new ArrayList<>();
+        //Collections.addAll(list, ArrayUtils.);
+
+        return new ResponseEntity<>(new Gson().toJson( Arrays.copyOf(users,20)).toString(),HttpStatus.OK);
 
         //return generalService.callWebService(url, HttpMethod.GET,null,true);
     }
 
-    @GetMapping(value="/user/serach/{searchCondition}")
+    @GetMapping(value="/user/search/{searchCondition}")
     public ResponseEntity<?> getByCondition(@PathVariable String searchCondition){
 
-        String url = "/OBISEC/api/v1.0/UserMng/Search/"+searchCondition+"?detail=true" ;
-
+        String url = "/OBISEC/api/v1.0/UserMng/Search?searchWord="+searchCondition ;
+        //http://172.25.40.135:9500/OBISEC/api/v1.0/UserMng/Search?searchWord=zahra
         return generalService.callWebService(url,HttpMethod.GET,null,true);
     }
 
